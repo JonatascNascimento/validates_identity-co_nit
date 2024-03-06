4dac24cbcbb8c0377acb9a246f23c91a1c032715
JonatascNascimento <jonatascn@gmail.com>
2024-03-05 22:48:26 -0300

    first commit

---
+++ lib/validates_identity/co_nit.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "co_nit/version"
+
+module ValidatesIdentity
+  module CoNit
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/validates_identity/co_nit_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ValidatesIdentity::CoNit do
+  it "has a version number" do
+    expect(ValidatesIdentity::CoNit::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
